f3514003840e9f3ebee27014422dbc3f1cdeb371
mahadi025 <mahadikarimmunif@gmail.com>
2023-07-03 13:37:21 +0600

    added select from gallery and progress bar

--- app/src/main/java/com/example/skindisease/MainActivity.java
+++ app/src/main/java/com/example/skindisease/MainActivity.java
@@ -1,151 +1,262 @@
 package com.example.skindisease;
 
-import android.Manifest;
-import android.content.Intent;
-import android.content.pm.PackageManager;
-import android.graphics.Bitmap;
+
+import android.content.Context;
 import android.net.Uri;
-import android.os.Build;
 import android.os.Bundle;
-import android.os.Environment;
-import android.provider.MediaStore;
-import android.webkit.JavascriptInterface;
+import android.view.View;
+import android.webkit.WebChromeClient;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
-import androidx.core.content.ContextCompat;
-import java.io.File;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.text.SimpleDateFormat;
-import java.util.Date;
-import java.util.Locale;
-
-import android.webkit.WebChromeClient;
 import android.webkit.WebSettings;
+import android.widget.ProgressBar;
+
+//import class for Uploading part start
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Build;
+import android.os.Environment;
+import android.os.Parcelable;
+import android.provider.MediaStore;
+import android.util.Log;
+import android.webkit.ValueCallback;
+import android.widget.Toast;
+import java.io.File;
+import java.io.IOException;
 
 
 public class MainActivity extends AppCompatActivity implements ActivityCompat.OnRequestPermissionsResultCallback {
 
-    private WebView browser;
-    private static final int REQUEST_CODE_CAMERA = 100;
-    private static final int REQUEST_CODE_GALLERY = 200;
-    private static final int REQUEST_CODE_PERMISSION = 300;
+    private WebView web;
+    private ProgressBar progressBar;
+    private String webUrl="https://subaru-nano-resolutions-netherlands.trycloudflare.com";
+    public Context context;
+
+    private static final String TAG = MainActivity.class.getSimpleName();
+
+    private static final int FILECHOOSER_RESULTCODE = 1;
+    private ValueCallback<Uri> mUploadMessage;
+    private Uri mCapturedImageURI = null;
+
+    // the same for Android 5.0 methods only
+    private ValueCallback<Uri[]> mFilePathCallback;
+    private String mCameraPhotoPath;
+
+    private static String file_type     = "*/*";
+    private String cam_file_data = null;
+    private ValueCallback<Uri> file_data;
+    private ValueCallback<Uri[]> file_path;
+    private final static int file_req_code = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        browser = findViewById(R.id.webView);
-        browser.setWebViewClient(new MyBrowser());
-        browser.setWebChromeClient(new WebChromeClient());
-        WebSettings webSettings = browser.getSettings();
-        webSettings.setJavaScriptEnabled(true);
-        webSettings.setAllowFileAccess(true);
-        webSettings.setAllowFileAccessFromFileURLs(true);
-        webSettings.setAllowUniversalAccessFromFileURLs(true);
-        browser.addJavascriptInterface(new WebViewInterface(), "AndroidInterface");
-        browser.loadUrl("https://health-privilege-maker-regulatory.trycloudflare.com");
+        web = findViewById(R.id.webView);
 
-        // Request storage permission if not granted
-        requestStoragePermission();
-    }
 
-    private class MyBrowser extends WebViewClient {
-        @Override
-        public boolean shouldOverrideUrlLoading(WebView view, String url) {
-            view.loadUrl(url);
-            return true;
-        }
-    }
 
-    private class WebViewInterface {
-        @JavascriptInterface
-        public void openCamera() {
-            // Open camera
-            Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-            startActivityForResult(intent, REQUEST_CODE_CAMERA);
-        }
-
-        @JavascriptInterface
-        public void openGallery() {
-            // Open gallery
-            Intent intent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
-            startActivityForResult(intent, REQUEST_CODE_GALLERY);
-        }
-    }
-
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-
-        if (requestCode == REQUEST_CODE_CAMERA && resultCode == RESULT_OK) {
-            // Handle camera result
-            Bitmap imageBitmap = (Bitmap) data.getExtras().get("data");
-            saveImage(imageBitmap);
-        } else if (requestCode == REQUEST_CODE_GALLERY && resultCode == RESULT_OK) {
-            // Handle gallery result
-            if (data != null) {
-                Uri selectedImageUri = data.getData();
-                Bitmap imageBitmap = null;
-                try {
-                    imageBitmap = MediaStore.Images.Media.getBitmap(this.getContentResolver(), selectedImageUri);
-                } catch (IOException e) {
-                    e.printStackTrace();
+        progressBar=findViewById(R.id.progress);
+        web.setWebChromeClient(new WebChromeClient(){
+            @Override
+            public void onProgressChanged(WebView view, int progress) {
+                progressBar.setProgress(progress);
+                if(progress<100 && progressBar.getVisibility()==ProgressBar.GONE){
+                    progressBar.setVisibility(ProgressBar.VISIBLE);
                 }
-                saveImage(imageBitmap);
+                if(progress==100){
+                    progressBar.setVisibility(ProgressBar.GONE);
+                }
             }
-        }
-    }
+            // for Lollipop, all in one
+            public boolean onShowFileChooser(
+                    WebView webView, ValueCallback<Uri[]> filePathCallback,
+                    WebChromeClient.FileChooserParams fileChooserParams) {
+                if (mFilePathCallback != null) {
+                    mFilePathCallback.onReceiveValue(null);
+                }
+                mFilePathCallback = filePathCallback;
 
-    private void saveImage(Bitmap imageBitmap) {
-        if (imageBitmap != null) {
-            // Save image to external storage
-            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M && ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
-                // Request storage permission if not granted
-                requestStoragePermission();
-            } else {
-                String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss", Locale.getDefault()).format(new Date());
-                String imageFileName = "IMG_" + timeStamp + ".jpg";
-                File storageDir = new File(Environment.getExternalStorageDirectory() + "/media/images/");
-                storageDir.mkdirs(); // Create the directory if it doesn't exist
-                if (storageDir.exists()) {
-                    File imageFile = new File(storageDir, imageFileName);
+                Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+
+                    // create the file where the photo should go
+                    File photoFile = null;
                     try {
-                        FileOutputStream fos = new FileOutputStream(imageFile);
-                        imageBitmap.compress(Bitmap.CompressFormat.JPEG, 100, fos);
-                        fos.flush();
-                        fos.close();
-                        // Image saved successfully, you can proceed with further operations
-                    } catch (IOException e) {
-                        e.printStackTrace();
+                        photoFile = createImageFile();
+                        takePictureIntent.putExtra("PhotoPath", mCameraPhotoPath);
+                    } catch (IOException ex) {
+                        // Error occurred while creating the File
+                        Log.e(TAG, "Unable to create Image File", ex);
+                    }
+
+                    // continue only if the file was successfully created
+                    if (photoFile != null) {
+                        mCameraPhotoPath = "file:" + photoFile.getAbsolutePath();
+                        takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT,
+                                Uri.fromFile(photoFile));
+                    } else {
+                        takePictureIntent = null;
+                    }
+                }
+
+                Intent contentSelectionIntent = new Intent(Intent.ACTION_GET_CONTENT);
+                contentSelectionIntent.addCategory(Intent.CATEGORY_OPENABLE);
+                contentSelectionIntent.setType("image/*");
+
+                Intent[] intentArray;
+                if (takePictureIntent != null) {
+                    intentArray = new Intent[]{takePictureIntent};
+                } else {
+                    intentArray = new Intent[0];
+                }
+
+                Intent chooserIntent = new Intent(Intent.ACTION_CHOOSER);
+                chooserIntent.putExtra(Intent.EXTRA_INTENT, contentSelectionIntent);
+                chooserIntent.putExtra(Intent.EXTRA_TITLE, getString(R.string.image_chooser));
+                chooserIntent.putExtra(Intent.EXTRA_INITIAL_INTENTS, intentArray);
+
+                startActivityForResult(chooserIntent, FILECHOOSER_RESULTCODE);
+
+                return true;
+            }
+
+            // creating image files (Lollipop only)
+            private File createImageFile() throws IOException {
+
+                File imageStorageDir = new File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES), "DirectoryNameHere");
+
+                if (!imageStorageDir.exists()) {
+                    imageStorageDir.mkdirs();
+                }
+
+                // create an image file name
+                imageStorageDir = new File(imageStorageDir + File.separator + "IMG_" + String.valueOf(System.currentTimeMillis()) + ".jpg");
+                return imageStorageDir;
+            }
+
+            // openFileChooser for Android 3.0+
+            public void openFileChooser(ValueCallback<Uri> uploadMsg, String acceptType) {
+                mUploadMessage = uploadMsg;
+
+                try {
+                    File imageStorageDir = new File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES), "DirectoryNameHere");
+
+                    if (!imageStorageDir.exists()) {
+                        imageStorageDir.mkdirs();
+                    }
+
+                    File file = new File(imageStorageDir + File.separator + "IMG_" + String.valueOf(System.currentTimeMillis()) + ".jpg");
+
+                    mCapturedImageURI = Uri.fromFile(file); // save to the private variable
+
+                    final Intent captureIntent = new Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE);
+                    captureIntent.putExtra(MediaStore.EXTRA_OUTPUT, mCapturedImageURI);
+                    // captureIntent.putExtra(MediaStore.EXTRA_SCREEN_ORIENTATION, ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+
+                    Intent i = new Intent(Intent.ACTION_GET_CONTENT);
+                    i.addCategory(Intent.CATEGORY_OPENABLE);
+                    i.setType("image/*");
+
+                    Intent chooserIntent = Intent.createChooser(i, getString(R.string.image_chooser));
+                    chooserIntent.putExtra(Intent.EXTRA_INITIAL_INTENTS, new Parcelable[]{captureIntent});
+
+                    startActivityForResult(chooserIntent, FILECHOOSER_RESULTCODE);
+                } catch (Exception e) {
+                    Toast.makeText(getBaseContext(), "Camera Exception:" + e, Toast.LENGTH_LONG).show();
+                }
+
+            }
+
+            // openFileChooser for Android < 3.0
+            public void openFileChooser(ValueCallback<Uri> uploadMsg) {
+                openFileChooser(uploadMsg, "");
+            }
+            public void openFileChooser(ValueCallback<Uri> uploadMsg, String acceptType, String capture) {
+                openFileChooser(uploadMsg, acceptType);
+            }
+
+
+        });
+        web.loadUrl(webUrl);
+        web.getSettings().setJavaScriptEnabled(true);
+        web.setWebViewClient(new WebViewClient());
+        web.getSettings().setAllowContentAccess(true);
+        web.getSettings().setAllowFileAccess(true);
+
+    }
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+
+        // code for all versions except of Lollipop
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.LOLLIPOP) {
+
+            if (requestCode == FILECHOOSER_RESULTCODE) {
+                if (null == this.mUploadMessage) {
+                    return;
+                }
+
+                Uri result = null;
+
+                try {
+                    if (resultCode != RESULT_OK) {
+                        result = null;
+                    } else {
+                        // retrieve from the private variable if the intent is null
+                        result = data == null ? mCapturedImageURI : data.getData();
+                    }
+                } catch (Exception e) {
+                    Toast.makeText(getApplicationContext(), "activity :" + e, Toast.LENGTH_LONG).show();
+                }
+
+                mUploadMessage.onReceiveValue(result);
+                mUploadMessage = null;
+            }
+
+        } // end of code for all versions except of Lollipop
+
+        // start of code for Lollipop only
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+
+            if (requestCode != FILECHOOSER_RESULTCODE || mFilePathCallback == null) {
+                super.onActivityResult(requestCode, resultCode, data);
+                return;
+            }
+
+            Uri[] results = null;
+
+            // check that the response is a good one
+            if (resultCode == Activity.RESULT_OK) {
+                if (data == null || data.getData() == null) {
+                    // if there is not data, then we may have taken a photo
+                    if (mCameraPhotoPath != null) {
+                        results = new Uri[]{Uri.parse(mCameraPhotoPath)};
                     }
                 } else {
-                    // Directory creation failed, handle this scenario accordingly
+                    String dataString = data.getDataString();
+                    if (dataString != null) {
+                        results = new Uri[]{Uri.parse(dataString)};
+                    }
                 }
             }
-        }
-    }
 
-    private void requestStoragePermission() {
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-            if (ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
-                ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, REQUEST_CODE_PERMISSION);
-            }
-        }
+            mFilePathCallback.onReceiveValue(results);
+            mFilePathCallback = null;
+
+        } // end of code for Lollipop only
     }
 
     @Override
-    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
-        if (requestCode == REQUEST_CODE_PERMISSION) {
-            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                // Permission granted, you can proceed with saving the image
-            } else {
-                // Permission denied, handle this scenario accordingly
-            }
+    public void onBackPressed() {
+        super.onBackPressed();
+        if (web.canGoBack()){
+            web.goBack();
+        }else{
+            super.onBackPressed();
         }
     }
+
 }
